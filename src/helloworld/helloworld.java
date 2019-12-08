b3a180e923b30efa4869e15edeedeb4091ad9483
Administrator <Administrator@DC-20160505VHPG>
2019-12-08 17:16:48 +0800

    test

--- src/helloworld/helloworld.java
+++ src/helloworld/helloworld.java
@@ -2,6 +2,6 @@ package helloworld;
 
 public class helloworld {
 	public static void main(String[] args) {
-		System.out.println("HelloWorld");
+		System.out.println("HelloWorld,¸ÄÄãÃÃµÄ´úÂë");
 	}
 }
